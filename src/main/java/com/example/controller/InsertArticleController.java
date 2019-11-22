dd32fc3d1be572e39419656cf15888750ce2da71
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2019-11-22 14:19:36 +0900

    InsertArticleControllerを完成しました

--- src/main/java/com/example/controller/InsertArticleController.java
+++ src/main/java/com/example/controller/InsertArticleController.java
@@ -1,18 +1,58 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-
-/**
- * 記事の追加を行うコントローラークラス.
- * @author yuichi
- *
- */
-=======
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
->>>>>>> feature/controller
+import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.form.CommentForm;
+import com.example.repository.ArticleRepository;
+import com.example.repository.CommentRepository;
+
 @Controller
 public class InsertArticleController {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@Autowired
+	private CommentRepository commentRepository;
+
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+
+	@ModelAttribute
+	public CommentForm setUpCommentForm() {
+		return new CommentForm();
+	}
+	
+	/**
+	 * 記事情報を挿入します.
+	 * 
+	 * @param form  情報が格納された器
+	 * @param model 情報を格納する器
+	 * @return 入力画面及び記事・コメント表示
+	 */
+	@RequestMapping("/insert-article")
+	public String insertArticle(@Validated ArticleForm form, BindingResult result, Model model) {
+		System.out.println(result);
+		if(result.hasErrors()) {
+			return "redirect:/";
+		}
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		articleRepository.insert(article);
+		model.addAttribute("article", article);
+
+		return "redirect:/article";
+	}
 
 }
