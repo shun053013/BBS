e5c64b90fc3678a503ea69ac577fc892a074df9c
yuichiyasui <yassan.b.a.w@gmail.com>
2019-11-22 13:59:00 +0900

    controllerパッケージを作成 ・ShowBbsControllerを作成 ・InsertArticleControllerを作成

---
+++ src/main/java/com/example/controller/InsertArticleController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 記事の追加を行うコントローラークラス.
+ * @author yuichi
+ *
+ */
+@Controller
+public class InsertArticleController {
+
+}
---
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 掲示板の記事一覧の表示を行うコントローラクラス.
+ * @author yuichi
+ *
+ */
+@Controller
+public class ShowBbsController {
+
+}
