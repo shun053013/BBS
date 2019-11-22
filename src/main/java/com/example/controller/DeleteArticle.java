37d608938d9b7ad70d1519e549fb1fdfcad338a7
shun053013 <shun053013@gmail.com>
2019-11-22 14:26:26 +0900

    DeleteArticleController作りました

---
+++ src/main/java/com/example/controller/DeleteArticle.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.ArticleRepository;
+import com.example.repository.CommentRepository;
+
+@Controller
+public class DeleteArticle {
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	
+	
+	@RequestMapping("/delete")
+	public String deleteArticle(Integer id) {
+		commentRepository.deleteByArticleId(id);
+		articleRepository.deleteById(id);
+		return "redirect:/article";
+	}
+	
+
+}
