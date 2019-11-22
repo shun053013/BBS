8f045f463fc0f965ba2fbe6b2aac14291a252792
yuichiyasui <yassan.b.a.w@gmail.com>
2019-11-22 14:23:13 +0900

    エラーが出たため再プッシュ

--- src/main/java/com/example/controller/InsertArticleController.java
+++ src/main/java/com/example/controller/InsertArticleController.java
@@ -1,17 +1,11 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-
 /**
  * 記事の追加を行うコントローラークラス.
  * @author yuichi
  *
  */
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-
->>>>>>> feature/controller
 @Controller
 public class InsertArticleController {
 
--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 掲示板の記事一覧の表示を行うコントローラクラス.
@@ -9,5 +10,15 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ShowBbsController {
-
+	
+	/**
+	 * 掲示板のトップページにフォワードするメソッド.
+	 * @return 掲示板のトップページ
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+	}
+	
+	
 }
